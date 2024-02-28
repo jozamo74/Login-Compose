6e6e5636ec2cd90ef7a46fd534dfc4f4bee1ef84
José Zambrano <jzambra1@tragsa.es>
2024-02-28 08:07:00 +0100

    Design footer

--- app/src/main/java/com/ttec/section13/ui/theme/LoginScreen.kt
+++ app/src/main/java/com/ttec/section13/ui/theme/LoginScreen.kt
@@ -44,6 +44,37 @@ fun LoginScreen() {
         
         Header(Modifier.align(Alignment.TopEnd))
         Body(Modifier.align(Alignment.Center))
+        Footer(Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+@Composable
+fun Footer(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Divider(
+            modifier = Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .fillMaxWidth()
+        ) 
+        Spacer(modifier = Modifier.size(24.dp))
+        SignUp()
+        Spacer(modifier = Modifier.size(24.dp))
+    }
+
+}
+
+@Composable
+fun SignUp() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(text = "Don't have an account?", fontSize = 12.sp, color = Color(0xFFb5b5b5))
+        Text(
+            text = "Sign up.",
+            modifier = Modifier.padding(horizontal = 8.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9)
+        )
     }
 }
 
